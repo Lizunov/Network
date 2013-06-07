5959d6d837068776ee51ae2e5e24049ef31246be
Igor Lziunov <igor.lizunov@icloud.com>
2013-06-07 12:17:09 +0300

    First commit of Network project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Network::Application.config.secret_token = '0db639267b2fd88e817124b71ee1db33dd242c7319707a6f335046e38914d30d6e8aae39f22483c213a159e3ee1b31f950a4091718ddb056e1354929a111f88d'
